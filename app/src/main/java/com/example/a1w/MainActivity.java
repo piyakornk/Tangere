6d6a60619f7ab37e790137b714209b337503a849
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2019-02-28 19:20:02 +0700

    test main page

--- app/src/main/java/com/example/a1w/MainActivity.java
+++ app/src/main/java/com/example/a1w/MainActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello from franc");
     }
 }
