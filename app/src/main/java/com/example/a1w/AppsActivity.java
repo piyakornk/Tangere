7cbaf1a22b6955e7c9d1c99739b29bdb37e189f0
piyakornk <pk.franc@hotmail.com>
2019-02-28 09:16:06 +0700

    AppsActivity

---
+++ app/src/main/java/com/example/a1w/AppsActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1w;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps2);
+    }
+}
